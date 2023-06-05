8aea1ee6c3ae3fd5d7b3c1eae61fecd4ea596161
Rufaida <rk3963@nyu.edu>
2023-06-05 15:28:16 -0400

    Revert "Updates"
    This reverts commit 8e7f02afcc230d5a225adabf0806468e9c8b46cd.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { NavBar } from "./NavBar";
+import { BannerMain } from "./BannerMain";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <BannerMain />
+    </div>
+  );
+}
+
+export default App;
